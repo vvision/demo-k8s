7ee5d79b4ee43e8b9e0187aa8d76b4d4420bc7be
Victor Voisin <victor.voisin@unicoda.fr>
2025-11-10 14:27:22 +0100

    feat: use node:25-alpine image

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 const color = process.env.COLOR || 'white';
 const podName = process.env.POD_NAME || 'unknown';
 
